cf26dc831e03148954fd998aa979efd4749a5cc9
Hannes Limmer <holzuhren-test@gmx.net>
2024-08-14 12:51:46 +0200

    added inAppBrowser

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { general, politics, sport } from '../data/newspaper-data';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,8 @@ import { general, politics, sport } from '../data/newspaper-data';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  private readonly iab = inject(InAppBrowser)
 
   private readonly _allNewspapers = [
     { title: 'Allgemein', data: general },
@@ -26,11 +30,16 @@ export class HomePage {
       return {
         ...np, data: np.data.filter(
           npData => npData.label.toLowerCase().includes(inputValue.toLowerCase()))
-
-
       }
-
 
     }).filter(np => np.data.length)
   }
+
+  public openLink(link: string) {
+    this.iab.create('https://' + link, '_self', {
+      location: 'no',
+      toolbar: 'yes'
+    });
+  };
+
 }
